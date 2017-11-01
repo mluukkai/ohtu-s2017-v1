005d265e4d85f38be703d8b69dac3acaaa6c698d
Matti Luukkainen <mluukkai@iki.fi>
2017-11-01 23:25:05 +0200

    koodia

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,17 @@
+package main;
+
+import ohtu.ohtuvarasto.Varasto;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        for (int i = 0; i < 1; i++) {
+            for (int j = 0; i < j; j++) {
+                System.out.println("virhe");
+            }
+        }
+    }
+}
